464d723683ce69e0ac25e55bfa3d5e4e36536016
nailtunayoksuz <toksuz@hotmail.com>
2021-11-18 01:15:08 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nail Tunay ÖKSÜZ on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
